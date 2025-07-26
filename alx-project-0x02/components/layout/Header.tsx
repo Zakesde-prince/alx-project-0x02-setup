638f6fcb7ab0d11800e01764a803e64607701b58
Zakesde-prince <prince.feela2905@gmail.com>
2025-07-26 08:45:54 +0200

    .

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,12 +1,18 @@
 import Link from 'next/link';
+import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-white shadow p-4 flex gap-4">
-      <Link href="/home">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/posts">Posts</Link>
-      <Link href="/users">Users</Link>
+    <header className="bg-blue-600 p-4 text-white flex justify-center gap-6">
+      <Link href="/" className="hover:underline">
+        Home
+      </Link>
+      <Link href="/home" className="hover:underline">
+        /home
+      </Link>
+      <Link href="/about" className="hover:underline">
+        /about
+      </Link>
     </header>
   );
 };
---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AboutPage() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-3xl font-bold">ℹ️ About This Project</h1>
+    </div>
+  );
+}
