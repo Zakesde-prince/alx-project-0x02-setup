596fe559dcd78a2ec386893acfbba36c1cf42fb0
Zakesde-prince <prince.feela2905@gmail.com>
2025-07-27 19:02:28 +0200

    .

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,16 +1,18 @@
 import Head from 'next/head';
 import Header from '@/components/layout/Header';
+import Card from '@/components/common/Card';
+
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Home</title>
-      </Head>
-      <Header />
-      <main className="flex min-h-screen items-center justify-center bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-600">Welcome to the Home Page</h1>
-      </main>
-    </>
+    <div>
+      <h1>Welcome to Home Page</h1>
+      
+      {/* Use Card component with some example props */}
+      <Card title="Hello" content="This is a reusable card component!" />
+      
+      <Card title="Another Card" content="You can use many cards with different info." />
+    </div>
   );
 }
+
